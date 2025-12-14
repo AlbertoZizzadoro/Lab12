14079aafa994cc3b9aa176adf38943281779914f
albizizza05 <alberto.zizzadoro@gmail.com>
2025-12-14 20:37:38 +0100

    completato il dao

--- database/dao.py
+++ database/dao.py
@@ -4,4 +4,34 @@ class DAO:
     """
     Implementare tutte le funzioni necessarie a interrogare il database.
     """
-    # TODO
+    @staticmethod
+    def get_all_rifugi():
+        conn=DBConnect.get_connection()
+        result=[]
+        cursor=conn.cursor(dictionary=True)
+        query="SELECT * FROM rifugio"
+        cursor.execute(query)
+        for row in cursor:
+            result.append(row)
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def get_all_connessioni(anno):
+        conn = DBConnect.get_connection()
+        result = []
+        cursor = conn.cursor(dictionary=True)
+
+
+        query = ("SELECT * FROM connessione "
+                 "WHERE anno <= %s")
+
+        cursor.execute(query, (anno,))
+
+        for row in cursor:
+            result.append(row)
+
+        cursor.close()
+        conn.close()
+        return result
